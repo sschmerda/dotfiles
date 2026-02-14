4b4fe2457abfb89a868afd886016ba833df90a1a
Stefan Schmerda <stefan.schmerda@gmail.com>
2026-02-14 17:27:00 +0100

    change nvim options and tmux conf in order to allow docker container clipboard pass data to hostmachine

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,21 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Allow terminal neovim to yank to host clipboard
+vim.opt.clipboard = "unnamedplus"
+
+local ok, osc52 = pcall(require, "vim.ui.clipboard.osc52")
+if ok then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+end
