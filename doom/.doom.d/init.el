15ca1aaeb0e5472b78860d646a24ae23b545656e
Stefan Schmerda <stefan.schmerda@gmail.com>
2026-04-28 16:33:16 +0200

    add docker support

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -95,7 +95,7 @@
        ;;collab            ; buffers with friends
        debugger          ; stepping through code, to help you add bugs
        ;;direnv
-       ;;docker
+       (docker +lsp +tree-sitter)
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
