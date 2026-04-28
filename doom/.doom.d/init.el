d14ccb9346ae51ab89ed0e2d0f06722062cd2a2b
Stefan Schmerda <stefan.schmerda@gmail.com>
2026-04-28 17:08:23 +0200

    add makefile support

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -104,7 +104,7 @@
        ;;llm               ; when I said you needed friends, I didn't mean...
        (lsp +peek)      ; M-x vscode
        magit             ; a git porcelain for Emacs
-       ;;make              ; run make tasks from Emacs
+       make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
        ;;terraform         ; infrastructure as code
