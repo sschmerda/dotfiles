9015a96f4b0555530d38de4d38ff7d83ba82a3a4
Stefan Schmerda <stefan.schmerda@gmail.com>
2026-04-28 15:34:21 +0200

    enable debugger

--- doom/.doom.d/init.el
+++ doom/.doom.d/init.el
@@ -93,7 +93,7 @@
        ;;ansible
        biblio            ; Writes a PhD for you (citation needed)
        ;;collab            ; buffers with friends
-       ;;debugger          ; stepping through code, to help you add bugs
+       debugger          ; stepping through code, to help you add bugs
        ;;direnv
        ;;docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
